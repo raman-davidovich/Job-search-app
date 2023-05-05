55262cc907fc5d5532f86ae0e0e20503feb35118
Raman Davidovich <raman.davidovich@gmail.com>
2023-05-05 17:47:02 +0300

    refactor: moved theme in detached file

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,9 @@
+import { Theme } from "./styles/theme";
 import { Button, Loader, MantineProvider, Text } from "@mantine/core";
 
 const App = () => {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        fontFamily: "Inter, sans-serif",
-        fontSizes: {
-          xs: "14px",
-          sm: "16px",
-          md: "20px",
-          lg: "24px",
-          xl: "28px",
-        },
-        radius: {
-          xs: "4px",
-          sm: "8px",
-          md: "12px",
-        },
-        colors: {
-          grey: [
-            "#FFFFFF",
-            "#F5F5F6",
-            "#EAEBED",
-            "#D5D6DC",
-            "#ACADB9",
-            "#7B7C88",
-            "#232134",
-          ],
-          blue: [
-            "#DEECFF",
-            "#C9E0FF",
-            "#B7D6FF",
-            "#92C1FF",
-            "#5E96FC",
-            "#3B7CD3",
-          ],
-        },
-      }}
-    >
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={Theme}>
       <Text>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ullam
         incidunt excepturi inventore ea illo, quos id eveniet eligendi sunt
