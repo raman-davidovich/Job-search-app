ff2d0df3465a142eabc57c011160113f4dafc4c2
Raman Davidovich <raman.davidovich@gmail.com>
2023-05-06 18:43:51 +0300

    refactor: changed position

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,27 +1,32 @@
 import GlobalSvgSelector from "../assets/icons/global/GlobalSvgSelector";
-import { Stack, Text, Button } from "@mantine/core";
+import { Center, Space, Stack, Text, Button } from "@mantine/core";
 
 const NotFoundPage = () => {
   return (
-    <Stack spacing="xl" align="center">
-      <GlobalSvgSelector id="not_found" />
-      <Text fz="lg" fw={700}>
-        Упс, здесь ещё ничего нет!
-      </Text>
-      <Button
-        variant="light"
-        color="blue"
-        fullWidth
-        mt="md"
-        radius="sm"
-        sx={(theme) => ({
-          fontFamily: "Open Sans, sans-serif",
-          color: theme.colors.blue[5],
-        })}
-      >
-        Поиск Вакансий
-      </Button>
-    </Stack>
+    <>
+      <Space h={120} />
+      <Center maw={328} mx="auto">
+        <Stack spacing="xl" align="center">
+          <GlobalSvgSelector id="not_found" />
+          <Text fz="lg" fw={700}>
+            Упс, здесь ещё ничего нет!
+          </Text>
+          <Button
+            variant="light"
+            color="blue"
+            fullWidth
+            mt="md"
+            radius="sm"
+            sx={(theme) => ({
+              fontFamily: "Open Sans, sans-serif",
+              color: theme.colors.blue[5],
+            })}
+          >
+            Поиск Вакансий
+          </Button>
+        </Stack>
+      </Center>
+    </>
   );
 };
 
