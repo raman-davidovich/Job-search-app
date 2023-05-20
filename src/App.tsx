f332ffbb62ad7e97cfda1b0074123639696b8849
Raman Davidovich <raman.davidovich@gmail.com>
2023-05-20 09:39:37 +0300

    feat: added classes for pagination control bar

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,32 @@ function GlobalStyles() {
   return (
     <Global
       styles={(Theme) => ({
+        ".pagination": {
+          fontFamily: "Roboto, sans-serif",
+          listStyle: "none",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "5rem",
+          fontSize: "1.2rem",
+          gap: "5px",
+
+          "& .page-num": {
+            padding: "8px 15px",
+            cursor: "pointer",
+            borderRadius: "3px",
+            fontWeight: 400,
+
+            "&:hover": {
+              backgroundColor: Theme.colors.blue[4],
+              color: Theme.colors.grey[4],
+            },
+          },
+
+          "& .active-num": {
+            backgroundColor: Theme.colors.blue[4],
+          },
+        },
         body: {
           backgroundColor: Theme.colors.grey[4],
         },
